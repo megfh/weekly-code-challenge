7fc2250b942cd10a37ad77b9fc1a159fa3dcbe74
Meghan Hannon <mhannon4@uwo.ca>
2019-10-13 12:29:57 -0400

    basic functionality done

---
+++ week-01/js/main.js
@@ -0,0 +1,26 @@
+var signin = true; 
+
+const signinForm = document.getElementById("signinForm"); 
+const signupForm = document.getElementById("signupForm"); 
+const signinOverlay = document.getElementById("signinOverlay"); 
+const signupOverlay = document.getElementById("signupOverlay"); 
+const signinButton = document.querySelector("#signinOverlay button"); 
+const signupButton = document.querySelector("#signupOverlay button"); 
+
+function signUp(e) {
+  signinForm.classList.add("hidden"); 
+  signinOverlay.classList.remove("hidden"); 
+  signupForm.classList.remove("hidden"); 
+  signupOverlay.classList.add("hidden"); 
+}
+
+function signIn(e) {
+  signinForm.classList.remove("hidden"); 
+  signinOverlay.classList.add("hidden"); 
+  signupForm.classList.add("hidden"); 
+  signupOverlay.classList.remove("hidden"); 
+}
+
+signinButton.addEventListener("click", signIn); 
+signupButton.addEventListener("click", signUp); 
+

bced258c84f95fb2bbea4e06f8506b47cd60c563
Meg <megfhannon@gmail.com>
2019-10-16 14:59:27 -0400

    challenge 1 complete

--- week-01/js/main.js
+++ week-01/js/main.js
@@ -6,23 +6,45 @@ const signinOverlay = document.getElementById("signinOverlay");
 const signupOverlay = document.getElementById("signupOverlay"); 
 const signinButton = document.querySelector("#signinOverlay button"); 
 const signupButton = document.querySelector("#signupOverlay button"); 
+const overlayContainer = document.querySelector('.overlay-container'); 
+const formContainer = document.querySelector('.form-container'); 
 
 function signUp(e) {
-  signinForm.classList.add("hidden"); 
+  // remove initial hidden class
   signinOverlay.classList.remove("hidden"); 
   signupForm.classList.remove("hidden"); 
-  signupOverlay.classList.add("hidden"); 
-  console.log("animating");
-  
-  // signinForm.classList.add("animateSignInForm"); 
-  // signupOverlay.classList.add("animateSignUpOverlay"); 
+  // remove previous animations
+  signinForm.classList.remove('show'); 
+  signupForm.classList.remove('hide'); 
+  signupOverlay.classList.remove('show'); 
+  signinOverlay.classList.remove('hide'); 
+  overlayContainer.classList.remove('return-right'); 
+  formContainer.classList.remove('return-left'); 
+
+  // add new animations
+  signinForm.classList.add('hide'); 
+  signupForm.classList.add('show'); 
+  signupOverlay.classList.add('hide'); 
+  signinOverlay.classList.add('show'); 
+  overlayContainer.classList.add('move-left'); 
+  formContainer.classList.add('move-right'); 
 }
 
 function signIn(e) {
-  signinForm.classList.remove("hidden"); 
-  signinOverlay.classList.add("hidden"); 
-  signupForm.classList.add("hidden"); 
-  signupOverlay.classList.remove("hidden"); 
+  // remove previous animations
+  signinForm.classList.remove('hide'); 
+  signupForm.classList.remove('show'); 
+  signupOverlay.classList.remove('hide'); 
+  signinOverlay.classList.remove('show'); 
+  overlayContainer.classList.remove('move-left'); 
+  formContainer.classList.remove('move-right'); 
+  // add current animations 
+  signinForm.classList.add('show'); 
+  signupForm.classList.add('hide'); 
+  signupOverlay.classList.add('show'); 
+  signinOverlay.classList.add('hide'); 
+  overlayContainer.classList.add('return-right'); 
+  formContainer.classList.add('return-left'); 
 }
 
 signinButton.addEventListener("click", signIn); 
